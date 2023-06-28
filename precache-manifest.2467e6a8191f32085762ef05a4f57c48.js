51e0bd1828bd6799ca2baf50b87b55583a03f741
Shubham Ubhe <ubheshubham.37@gmail.com>
2023-06-28 13:47:27 +0530

    Updates

---
+++ precache-manifest.2467e6a8191f32085762ef05a4f57c48.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "498456b6727dc9123c70",
+    "url": "/resumeportfolio/static/css/main.f9ea27cb.chunk.css"
+  },
+  {
+    "revision": "498456b6727dc9123c70",
+    "url": "/resumeportfolio/static/js/main.28f6e837.chunk.js"
+  },
+  {
+    "revision": "5fc329c889bea90985bc",
+    "url": "/resumeportfolio/static/js/runtime~main.65f7733e.js"
+  },
+  {
+    "revision": "66acd1420e41bd937645",
+    "url": "/resumeportfolio/static/css/2.7de17a01.chunk.css"
+  },
+  {
+    "revision": "66acd1420e41bd937645",
+    "url": "/resumeportfolio/static/js/2.1d2b88f6.chunk.js"
+  },
+  {
+    "revision": "1998d2b56f1a3017ca83",
+    "url": "/resumeportfolio/static/js/3.5a9d5fa9.chunk.js"
+  },
+  {
+    "revision": "028e0b01ee2334428394e04f6f99b5a1",
+    "url": "/resumeportfolio/static/media/avatar.028e0b01.png"
+  },
+  {
+    "revision": "36850dffe5bc11d229a94a88db491d51",
+    "url": "/resumeportfolio/static/media/proj1.36850dff.jpg"
+  },
+  {
+    "revision": "06e7a00cfdcdb2bf5a8213547a169743",
+    "url": "/resumeportfolio/static/media/proj2.06e7a00c.png"
+  },
+  {
+    "revision": "fc8028c73c6eb2ba98cc5689d7e07183",
+    "url": "/resumeportfolio/static/media/proj3.fc8028c7.webp"
+  },
+  {
+    "revision": "d6eff5fe46c7e96e0fa990ca4ef86c67",
+    "url": "/resumeportfolio/static/media/proj4.d6eff5fe.jpg"
+  },
+  {
+    "revision": "699fb3e0b2d401e9786bd70dde2572cd",
+    "url": "/resumeportfolio/static/media/proj5.699fb3e0.jpg"
+  },
+  {
+    "revision": "4298df1344981d908d21d50e1063b229",
+    "url": "/resumeportfolio/static/media/proj6.4298df13.jpeg"
+  },
+  {
+    "revision": "27e9c9a7f95414ce3baaf201b99f88ca",
+    "url": "/resumeportfolio/index.html"
+  }
+];
