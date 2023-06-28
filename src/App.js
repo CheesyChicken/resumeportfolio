2f2b4aca58613c2e02f92a3e47d6ca929e77161d
Shubham Ubhe <ubheshubham.37@gmail.com>
2023-06-28 13:54:37 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
@@ -9,20 +9,33 @@ import Footer from './components/Footer/Footer';
 import ProjectDisplay from './pages/ProjectDisplay/ProjectDisplay';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
-          <Route path="/experience" element={<Experience />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Router >
+        <
+        Navbar / >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/projects"
+        element = { < Projects / > }
+        /> <
+        Route path = "/project/:id"
+        element = { < ProjectDisplay / > }
+        /> <
+        Route path = "/experience"
+        element = { < Experience / > }
+        /> <
+        /Routes> <
+        Footer / >
+        <
+        /Router> <
+        /div>
+    );
 }
 
 export default App;
